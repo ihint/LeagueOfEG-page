2e08e150d7da190f59ffb9b9a62f83978540c2a9
Ian <Ihint07@gmail.com>
2022-08-20 22:06:42 -0400

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "651519304948506624"; // your league ID
+export const leagueID = "839974480980836352"; // your league ID
 export const leagueName = "League of Extraordinary Gentleman"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = false; // true for dynasty leagues, false for redraft and keeper
